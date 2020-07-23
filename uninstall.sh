30085cdc2f35d64b5577a54b5f161ac19274354e
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2020-07-23 22:25:20 +0530

    removed sudo from uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,18 +7,18 @@ if [ "$REPLY" != "yes" ]; then
 fi
 
 echo Stopping powertop service...
-sudo systemctl stop pwrtp.service
+systemctl stop pwrtp.service
 
 echo Disabling powertop service...
-sudo systemctl disable pwrtp.service
+systemctl disable pwrtp.service
 
 echo Removing powertop service...
-sudo rm /etc/systemd/system/pwrtp.service
+rm /etc/systemd/system/pwrtp.service
 
 echo Removing powertop script...
-sudo rm /usr/bin/pwrtp
+rm /usr/bin/pwrtp
 
 echo Reloading systemd...
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 echo Done
