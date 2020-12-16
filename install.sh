2f35a1143119416d1ce05e4066f08ce981326b8c
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2020-12-16 18:37:02 +0530

    made output more readable

--- install.sh
+++ install.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
 
-printf "\nPowertop script and service installer\n"
+text () { printf "\n\n\n$1\n" ; }
+
+text "POWERTOP AUTOSTART INSTALLER"
 read -p " > Type 'yes' to proceed: "
 if [ "$REPLY" != "yes" ]; then
    exit
 fi
 
-echo Installing powertop script...
-cp ./pwrtp.sh /usr/bin/
+text "Installing powertop script..."
+cp -iv ./pwrtp.sh /usr/bin/
 chmod +x /usr/bin/pwrtp.sh
 
-echo Adding powertop service...
-cp ./pwrtp.service /etc/systemd/system/
+text "Installing powertop service..."
+cp -iv ./pwrtp.service /etc/systemd/system/
 
-echo Reloading systemd...
-systemctl daemon-reload
-
-echo Enabling powertop service...
+text "Enabling powertop service..."
 systemctl enable pwrtp.service
 
-echo Starting powertop service...
+text "Starting powertop service..."
 systemctl start pwrtp.service
 
-echo Reloading systemd...
+text "Reloading systemd daemon..."
 systemctl daemon-reload
 
-echo Done
+text "Done"
